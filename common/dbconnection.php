e4a4a8961912d596f087313fef3f0768158178b4
RobbeGT3 <2126902@talnet.nl>
2026-04-07 11:14:30 +0200

    DB connection file aangepast
    dbconnection file aangepast zodat die verbinding maakt met de database via AWS

--- common/dbconnection.php
+++ common/dbconnection.php
@@ -3,16 +3,32 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$servername = "127.0.0.1";
-$username = "root";
-$password = "LKP908H1";
-
-// $servername = "voedselhulpmaaskantje.cj2yck2ymf52.eu-north-1.rds.amazonaws.com";
-// $username = "MaasAdmin";
-// $password = "MaasKantje";
+$servername = "voedselhulpmaaskantje.cj2yck2ymf52.eu-north-1.rds.amazonaws.com";
+$username = "MaasAdmin";
+$password = "MaasKantje";
 $dbname = "Examenopdracht";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = mysqli_init();
+
+mysqli_ssl_set(
+    $conn,
+    NULL,
+    NULL,
+    __DIR__. "/rds-ca.pem",
+    NULL,
+    NULL
+);
+
+mysqli_real_connect(
+    $conn,
+    $servername,
+    $username,
+    $password,
+    $dbname,
+    3306,
+    NULL,
+    MYSQLI_CLIENT_SSL
+);
 
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
--- index.php
+++ index.php
@@ -13,7 +13,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Voedselbank Systeem</title>
 
-  <link rel="stylesheet" href="styles/login.css" />
+  <link rel="stylesheet" href="styles/styles.css" />
 
   <link
     rel="stylesheet"
