48d7fd5abdb93d300ef3e967057c4312d8c23b42
RobbeGT3 <2126902@talnet.nl>
2026-04-22 17:03:41 +0200

    Update zoekfunctie

--- voedselpakketten.php
+++ voedselpakketten.php
@@ -17,6 +17,7 @@ require_once  __DIR__. '/common/dbconnection.php';
     <title>Voedselpakketten</title>
     <link rel="stylesheet" href="styles/styles.css">
     <link rel="stylesheet" href="styles/voedselpakketten.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body>
 
